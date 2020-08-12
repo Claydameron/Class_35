fea88b8d2c35230f4f74385741907a5d9b7ba501
Claydameron <66542550+Claydameron@users.noreply.github.com>
2020-08-12 16:00:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var database;
+var form,game,player,playerCount,canvas;
+var gameState = 0
+
+function setup(){
+  canvas = createCanvas(400,400);
+    
+   database = firebase.database();
+
+   game = new Game();
+   game.getState();
+   game.start();
+}
+
+function draw(){
+}
+
+
+
